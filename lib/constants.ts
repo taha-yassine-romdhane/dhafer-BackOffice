0a43c1839938ce4459b179697ec73cf20227faa9
taha yassine romdhane <taha.romdhane1999@gmail.com>
2025-03-21 00:29:31 +0100

    tartil 2

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 // constants.ts
 export const SIZE_GROUPS: Record<string, string[]> = {
-    Femme: ["XS", "S", "M", "L", "XL", "XXL", "3XL", "4XL","36","38","40","42","44","46","48","50","52" ],
+    Femme: ["XS", "S", "M", "L", "XL", "XXL", "3XL", "4XL","36","38","40","42","44","46","48","50","52", "M-38", "L-40", "XL-42", "XXL-44", "3XL-46", "4XL-48" ],
     Enfants: ["2ans", "4ans", "6ans", "8ans", "10ans", "12ans"],
     Accessoires: ["Standard"]
   };
